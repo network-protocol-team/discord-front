b9f3af3e622c96d166dfe58fccc19198e2b976c2
monognuisy <ysmsmart1@gmail.com>
2024-05-19 22:30:53 +0900

    feat: websocket 초기 설정

--- src/components/VideoChatRoom.jsx
+++ src/components/VideoChatRoom.jsx
@@ -1,8 +1,79 @@
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
 import MicIcon from '@mui/icons-material/Mic';
 import CallEndIcon from '@mui/icons-material/CallEnd';
+import { useChatStore, useSocketStore } from '../data/store';
+import SockJS from 'sockjs-client';
+import { Client } from '@stomp/stompjs';
+import { useCallback, useEffect } from 'react';
 
 export default function VideoChatRoom() {
+  const videoSocket = useSocketStore((state) => state.videoSocket);
+  const setVideoSocket = useSocketStore((state) => state.setVideoSocket);
+
+  const selectedId = useChatStore((state) => state.selectedId);
+  const nickName = useChatStore((state) => state.nickName);
+
+  const connectVideoSocket = useCallback(() => {
+    const socket = new SockJS(import.meta.env.VITE_SOCK_URL);
+    const camKey = nickName;
+    setVideoSocket(
+      new Client({
+        webSocketFactory: () => socket,
+        onConnect: () => {
+          console.log('Connected to video webSocket');
+
+          // subscribe api 작성
+
+          videoSocket.subscribe(
+            `/sub/channels/${selectedId}/video/offer/${camKey}`,
+            onOffer,
+          );
+          videoSocket.subscribe(
+            `/sub/channels/${selectedId}/video/answer/${camKey}`,
+            onAnswer,
+          );
+          videoSocket.subscribe(
+            `/sub/channels/${selectedId}/video/ice-candidate/${camKey}`,
+            onCandidate,
+          );
+          videoSocket.subscribe(
+            `/sub/channels/${selectedId}/video/call-members`,
+            onMembers,
+          );
+          videoSocket.subscribe(
+            `/sub/channels/${selectedId}/video/receive-other`,
+            onOthers,
+          );
+
+          // TODO: publish 로직 작성
+        },
+        onDisconnect: () => {
+          console.log('Disconnected from video webSocket');
+        },
+      }),
+    );
+    videoSocket.activate();
+  }, [selectedId, nickName, videoSocket, setVideoSocket]);
+
+  const onOffer = () => {};
+  const sendOffer = () => {};
+  const onAnswer = () => {};
+  const sendAnswer = () => {};
+  const onCandidate = () => {};
+  const sendCandidate = () => {};
+  const onMembers = () => {};
+  const sendMembers = () => {};
+  const onOthers = () => {};
+  const sendSelf = () => {};
+  const endCall = () => {};
+
+  useEffect(() => {
+    connectVideoSocket();
+    return () => {
+      endCall();
+    };
+  }, []);
+
   return (
     <div className="video-chat-wrapper">
       <div className="video-grid">
